beacc227bfaec94147f359a31d801fe80c5665c2
meghana2367 <maggy.chinta@gmail.com>
2024-07-14 21:30:14 -0500

    Added a hard coded card object to start off from

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar.js";
 import Hero from "./components/Hero.js";
+import Card from "./components/Card.js";
 
 export default function App() {
   return (
     <div id = "app">
       <Navbar />
       <Hero />
+      <Card />
     </div>
   )
 }
---
+++ airbnb/src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Card() {
+    return (
+        <div>
+            <img src = "/katie.png" alt = ""/>
+            <div>
+                <img src = "/star.jpg" alt = ""/>
+                <p>5.0</p>
+                <p>(6)*USA</p>
+            </div>
+            <p>Life lessons with Katie Zafares</p>
+            <p>From $136 / person</p>
+        </div>
+    )
+}
